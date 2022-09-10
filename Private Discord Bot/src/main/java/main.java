8487ddcc0e05011ea4428f9c29a9e6c74e6e2514
AvanderLugt <42913113+andre7455@users.noreply.github.com>
2022-09-10 19:30:34 +0200

    airds

--- Private Discord Bot/src/main/java/main.java
+++ Private Discord Bot/src/main/java/main.java
@@ -1,5 +1,10 @@
+import net.dv8tion.jda.api.JDABuilder;
 public class main {
     public static void main(String[] args) {
-        System.out.println("");
+        JDABuilder builder = JDABuilder.createDefault(args[0]);
+
+
+
+        builder.build();
     }
 }
