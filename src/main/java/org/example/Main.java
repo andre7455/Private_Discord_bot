ddb054c8590736da7a4b93be87b0f928769a8805
AvanderLugt <42913113+andre7455@users.noreply.github.com>
2022-09-15 16:25:21 +0200

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,7 @@
 package org.example;
 public class Main {
     public static void main(String[] args) {
-        discordInteractionsWrapper discord = new discordInteractionsWrapper();
+        DiscordInteractionsWrapper discord = new DiscordInteractionsWrapper();
         discord.test("kaas");
     }
 }
