627a82068860d9f3c974f1dd20fa35c1c1de827f
AvanderLugt <42913113+andre7455@users.noreply.github.com>
2022-09-15 16:24:08 +0200

    again

--- src/main/java/org/example/discordInteractionsWrapper.java
+++ src/main/java/org/example/discordInteractionsWrapper.java
@@ -1,6 +1,6 @@
 package org.example;
 
-public class discordInteractionsWrapper {
+public class DiscordInteractionsWrapper {
     public void test(String test) {
         System.out.println(test + test + test);
     }
