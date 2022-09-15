9cc7d312ebb7d6572b319b4cf55774dc08c449b1
AvanderLugt <42913113+andre7455@users.noreply.github.com>
2022-09-15 15:47:23 +0200

    test with external files

--- Private_Discord_bot/src/main/java/org/example/Main.java
+++ Private_Discord_bot/src/main/java/org/example/Main.java
@@ -2,7 +2,9 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        discordInteractionsWrapper DiscordWrapper = new discordInteractionsWrapper();
 
-
+        
+        DiscordWrapper.test("anus");
     }
 }
--- Private_Discord_bot/src/main/java/org/example/discordInteractionsWrapper.java
+++ Private_Discord_bot/src/main/java/org/example/discordInteractionsWrapper.java
@@ -1,4 +1,7 @@
 package org.example;
 
 public class discordInteractionsWrapper {
+    public void test(String test){
+        System.out.println(test);
+    }
 }
