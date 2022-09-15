9c2a451149b174c80238a7d9e55e1d658c6f82a3
AvanderLugt <42913113+andre7455@users.noreply.github.com>
2022-09-15 16:39:58 +0200

    looking if being even more explicit helps

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,7 @@
 package org.example;
 public class Main {
     public static void main(String[] args) {
-        org.example.DiscordInteractionsWrapper discord = new DiscordInteractionsWrapper();
+        org.example.DiscordInteractionsWrapper discord = new  org.example.DiscordInteractionsWrapper();
         discord.test("kaas");
     }
 }
