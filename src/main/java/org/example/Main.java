e884f391bf26cd47f053df7f1dccf54223d8bb22
AvanderLugt <42913113+andre7455@users.noreply.github.com>
2022-09-15 16:38:30 +0200

    test if being more explicit helps

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,7 @@
 package org.example;
 public class Main {
     public static void main(String[] args) {
-        DiscordInteractionsWrapper discord = new DiscordInteractionsWrapper();
+        org.example.DiscordInteractionsWrapper discord = new DiscordInteractionsWrapper();
         discord.test("kaas");
     }
 }
