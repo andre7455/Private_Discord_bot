8e3e670f784138165e05297c28cf1ed30d400651
AvanderLugt <42913113+andre7455@users.noreply.github.com>
2022-09-15 16:48:01 +0200

    lets just use it like this for now

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,14 @@
 package org.example;
 public class Main {
     public static void main(String[] args) {
-        org.example.DiscordInteractionsWrapper discord = new  org.example.DiscordInteractionsWrapper();
-        discord.test("kaas");
+
+        test("kaas ");
     }
+
+    public static void test(String temp) {
+        System.out.println(temp + temp + temp);
+    }
+
+
+
 }
