875102e0d74cfd2b69685388fa82ca4d66ef22f7
AvanderLugt <42913113+andre7455@users.noreply.github.com>
2022-09-15 15:38:09 +0200

    try 2th file addition

--- Private_Discord_bot/src/main/java/org/example/Main.java
+++ Private_Discord_bot/src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello andre, i'm updated and ready to be automated!");
+
+
     }
 }
