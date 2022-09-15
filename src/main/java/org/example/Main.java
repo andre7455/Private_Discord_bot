faba7b7b5694271a09720d0c461c47d4c3221ddb
AvanderLugt <42913113+andre7455@users.noreply.github.com>
2022-09-15 16:54:45 +0200

    other way of doing it

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,12 @@
 package org.example;
+import  org.example.*;
 public class Main {
     public static void main(String[] args) {
         run();
     }
 
     public static void run() {
-        org.example.DiscordInteractionsWrapper discord = new DiscordInteractionsWrapper();
+        DiscordInteractionsWrapper discord = new DiscordInteractionsWrapper();
         discord.test("worst ");
     }
 }
