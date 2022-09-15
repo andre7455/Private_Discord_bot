da651b216ee3217f3d7e6888a1632ba75c66f1af
AvanderLugt <42913113+andre7455@users.noreply.github.com>
2022-09-15 22:33:02 +0200

    update and disabled discord file

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,11 @@ public class Main {
     }
 
     public static void run() {
-        DiscordInteractionsWrapper discord = new DiscordInteractionsWrapper();
-        discord.test("worst ");
+        //DiscordInteractionsWrapper discord = new DiscordInteractionsWrapper();
+        discord("worst ");
+    }
+
+    public static void discord(String test) {
+        System.out.println(test + test + test);
     }
 }
