5f736280663b7372c3b0144e055a6a33de2acfb7
AvanderLugt <42913113+andre7455@users.noreply.github.com>
2022-09-18 00:33:02 +0200

    a lot of updates, almost a bot tho

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,7 @@
 package org.example;
-import  org.example.*;
+
 public class Main {
     public static void main(String[] args) {
-        run();
-    }
-
-    public static void run() {
-        //DiscordInteractionsWrapper discord = new DiscordInteractionsWrapper();
-        discord("i hope its not saturday ");
-    }
-
-    public static void discord(String test) {
-        System.out.println(test + test + test);
+        System.out.println("kaas");
     }
 }
