e383862cd1967ec7abf019df5a9d60d6af887375
AvanderLugt <42913113+andre7455@users.noreply.github.com>
2022-10-26 19:25:12 +0200

    test upload

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,92 +1,15 @@
-/*
- * This file is part of Discord4J.
- *
- * Discord4J is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Discord4J is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with Discord4J. If not, see <http://www.gnu.org/licenses/>.
- */
+package org.example;
+import java.util.Scanner;
 
-package discord4j.core;
-
-import discord4j.core.event.ReactiveEventAdapter;
-import discord4j.core.event.domain.interaction.ChatInputInteractionEvent;
-import discord4j.core.object.command.ApplicationCommandInteraction;
-import discord4j.core.object.command.ApplicationCommandInteractionOption;
-import discord4j.core.object.command.ApplicationCommandInteractionOptionValue;
-import discord4j.core.object.command.ApplicationCommandOption;
-import discord4j.core.support.GuildCommandRegistrar;
-import discord4j.discordjson.json.ApplicationCommandOptionData;
-import discord4j.discordjson.json.ApplicationCommandRequest;
-import org.reactivestreams.Publisher;
-import reactor.core.publisher.Mono;
-import reactor.util.Logger;
-import reactor.util.Loggers;
-
-import java.util.Collections;
-import java.util.Random;
-
-public class ExampleChatInputInteractionEvent {
-
-    private static final Logger log = Loggers.getLogger(ExampleChatInputInteractionEvent.class);
-
-    private static final String token = System.getenv("token");
-    private static final long guildId = Long.parseLong(System.getenv("guildId"));
-
+public class Main{
     public static void main(String[] args) {
-        GatewayDiscordClient client = DiscordClient.create(token)
-                .login()
-                .block();
-
-        ApplicationCommandRequest randomCommand = ApplicationCommandRequest.builder()
-                .name("random")
-                .description("Send a random number")
-                .addOption(ApplicationCommandOptionData.builder()
-                        .name("digits")
-                        .description("Number of digits (1-20)")
-                        .type(ApplicationCommandOption.Type.INTEGER.getValue())
-                        .required(false)
-                        .build())
-                .build();
-
-        GuildCommandRegistrar.create(client.getRestClient(), guildId, Collections.singletonList(randomCommand))
-                .registerCommands()
-                .doOnError(e -> log.warn("Unable to create guild command", e))
-                .onErrorResume(e -> Mono.empty())
-                .blockLast();
-
-        client.on(new ReactiveEventAdapter() {
-
-            private final Random random = new Random();
-
-            @Override
-            public Publisher<?> onChatInputInteraction(ChatInputInteractionEvent event) {
-                if (event.getCommandName().equals("random")) {
-                    String result = result(random, event.getInteraction().getCommandInteraction().get());
-                    return event.reply(result);
-                }
-                return Mono.empty();
-            }
-        }).blockLast();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("kaas is nu nieuw");
+        System.out.println("wat is getal 1?");
+        int inputEen = scanner.nextInt();
+        System.out.println("Wat is getal 2");
+        int inputTwee = scanner.nextInt();
+        System.out.println(inputEen + " + " + inputTwee + " = "+ TestClass.Add(inputTwee, inputTwee));
     }
 
-    private static String result(Random random, ApplicationCommandInteraction acid) {
-        long digits = acid.getOption("digits")
-                .flatMap(ApplicationCommandInteractionOption::getValue)
-                .map(ApplicationCommandInteractionOptionValue::asLong)
-                .orElse(1L);
-        StringBuilder result = new StringBuilder();
-        for (int i = 0; i < Math.max(1, Math.min(20, digits)); i++) {
-            result.append(random.nextInt(10));
-        }
-        return result.toString();
-    }
 }
---
+++ src/main/java/org/example/TestClass.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class TestClass {
+    public static int Add(int one, int two){
+        return one + two;
+    }
+}
