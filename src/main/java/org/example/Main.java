37f0b82b8e1deeb4128b3c3b2eeed237637fd374
AvanderLugt <42913113+andre7455@users.noreply.github.com>
2022-11-04 14:44:55 +0100

    update test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,22 @@
 package org.example;
+
 import java.util.Scanner;
 
-public class Main{
+public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("kaas is nu nieuw");
-        System.out.println("wat is getal 1?");
-        int inputEen = scanner.nextInt();
-        System.out.println("Wat is getal 2");
-        int inputTwee = scanner.nextInt();
-        System.out.println(inputEen + " + " + inputTwee + " = "+ TestClass.Add(inputEen, inputTwee));
+        boolean running = true;
+        while (running = true) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Wil je iets uitrekenen?");
+            if (scanner.nextLine().equals("nee")){
+                running = false;
+                break;
+            }
+            System.out.println("wat is getal 1?");
+            int inputEen = scanner.nextInt();
+            System.out.println("Wat is getal 2");
+            int inputTwee = scanner.nextInt();
+            System.out.println(inputEen + " + " + inputTwee + " = " + TestClass.Add(inputEen, inputTwee));
+        }
     }
-
 }
