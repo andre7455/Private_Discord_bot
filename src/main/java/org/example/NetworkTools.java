d36cc182795a0ad5d167902c8198ae9a9194e739
AvanderLugt <42913113+andre7455@users.noreply.github.com>
2022-12-14 13:20:25 +0100

    making it working again

--- src/main/java/org/example/NetworkTools.java
+++ src/main/java/org/example/NetworkTools.java
@@ -6,7 +6,7 @@ public class NetworkTools {
     public void checkInternetStatus(){
         try {
             // Get the InetAddress for the specified domain
-            InetAddress address = InetAddress.getByName("ksjdhfgkdjhf");
+            InetAddress address = InetAddress.getByName("www.discord.com");
 
             // Check if the address is reachable
             if (address.isReachable(5000)) {
