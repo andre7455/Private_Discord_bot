5dd7ebc9cc0d62edb4229ecdc0f0aae10637e6a5
AvanderLugt <42913113+andre7455@users.noreply.github.com>
2022-12-14 13:23:40 +0100

    Making some small changes

---
+++ src/main/java/org/example/GeneralTools.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class GeneralTools {
+    public void waitTillInput(){
+        Scanner scanner = new Scanner(System.in);
+    }
+}
--- src/main/java/org/example/NetworkTools.java
+++ src/main/java/org/example/NetworkTools.java
@@ -4,19 +4,21 @@ import java.net.InetAddress;
 
 public class NetworkTools {
     public void checkInternetStatus(){
+        GeneralTools generalTools = new GeneralTools();
         try {
             // Get the InetAddress for the specified domain
             InetAddress address = InetAddress.getByName("www.discord.com");
-
             // Check if the address is reachable
             if (address.isReachable(5000)) {
                 System.out.println("The domain is reachable.") ;
+                generalTools.waitTillInput();
             } else {
                 System.out.println("The domain is not reachable.");
+                generalTools.waitTillInput();
             }
         } catch (Exception e) {
             e.printStackTrace();
+            generalTools.waitTillInput();
         }
-
     }
 }
