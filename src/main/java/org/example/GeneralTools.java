03659bd16258e96e25bce3846257b98dd98c58c6
AvanderLugt <42913113+andre7455@users.noreply.github.com>
2022-12-14 21:06:52 +0100

    forgot to actually wait for the input, only initialised the variables

--- src/main/java/org/example/GeneralTools.java
+++ src/main/java/org/example/GeneralTools.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class GeneralTools {
     public void waitTillInput() {
         Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
     }
     public void ClearTheTerminal(){
         System.out.print("\033[H\033[2J");
